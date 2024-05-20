43859ecfeb43fa0ed46d035174180c8e0cb7983f
willtpa <141321189+willtpa@users.noreply.github.com>
2024-05-20 09:54:01 +0800

    feat: add currency rate ui (with sample data) (#4)
    * feat: add currency rate ui (with sample data)
    
    * chore
    
    * chore: cleanup

---
+++ src/lib/components/currency-rate.svelte
@@ -0,0 +1,21 @@
+<!-- ChildComponent.svelte -->
+<script>
+	export let ticker;
+	export let color;
+	export let rate_base_quote;
+	export let id;
+</script>
+
+<div>
+	<div class="border rounded p-4 mb-4">
+		<h3 class="text-xl mb-2">
+			{ticker}/USD:
+			<span
+				class="{color === 'green'
+					? 'bg-green-500'
+					: 'bg-red-500'} text-black p-2 rounded bg-opacity-50">{rate_base_quote}</span
+			>
+		</h3>
+		<p class="text-black p-2 rounded max-w-sm">{id}</p>
+	</div>
+</div>
--- src/routes/rates/+page.svelte
+++ src/routes/rates/+page.svelte
@@ -1,22 +1,72 @@
 <script lang="ts">
+	/* eslint-disable @typescript-eslint/no-explicit-any */
 	import { onMount } from 'svelte';
+	import CurrencyRateComponent from '$components/currency-rate.svelte';
 
-	export let data;
-	let messages: unknown[] = [];
+	let currency: any = null;
+	let currencyGrp: Record<string, any> = {
+		BTC: null,
+		ETH: null,
+		SOL: null,
+		MATIC: null
+	};
 
-	onMount(() => {
-		const sse = new EventSource(`${data.sseCurrencyRates}`);
+	async function readFile(filePath: string) {
+		try {
+			const content = await fetch(filePath);
+			const result = await content.text();
+			return JSON.parse(result);
+		} catch (error) {
+			console.error('Error reading file:', error);
+		}
+	}
 
-		sse.onmessage = (rate) => {
-			messages = [...messages, rate.data];
-		};
-	});
+	function upOrDown(targetCurrency: any, currency: any) {
+		if (!targetCurrency) {
+			return;
+		}
+		let color = 'green';
+		if (targetCurrency.rate_base_quote > currency.rate_base_quote) {
+			color = 'red';
+		}
+		return color;
+	}
+
+	function updateCurrencyGrp(currencyGrp: Record<string, any>, currency: any) {
+		Object.keys(currencyGrp).forEach((key) => {
+			if (currency.quote === key) {
+				const color = upOrDown(currencyGrp[key], currency);
+				currencyGrp[key] = { ...(currencyGrp[key] || {}), ...currency, ...{ color } };
+			}
+		});
+		return currencyGrp || {};
+	}
+
+	async function subscribeToData() {
+		const data = await readFile('currencyrates.sample.json');
+		setInterval(
+			() => {
+				const rnd = Math.floor(Math.random() * data.length);
+				currency = { ...data[rnd] };
+
+				if (currency) {
+					currencyGrp = { ...(currencyGrp || {}), ...updateCurrencyGrp(currencyGrp, currency) };
+				}
+			},
+			Math.floor(Math.random() * 1_000) + 100
+		);
+	}
+
+	onMount(subscribeToData);
 </script>
 
-<h1>Rate Page</h1>
-
-<ul>
-	{#each messages as message}
-		<li>{message}</li>
-	{/each}
-</ul>
+{#each Object.entries(currencyGrp) as [key]}
+	{#if currencyGrp[key]}
+		<CurrencyRateComponent
+			ticker={currencyGrp[key].quote}
+			rate_base_quote={currencyGrp[key].rate_base_quote}
+			id={currencyGrp[key].id}
+			color={currencyGrp[key].color}
+		/>
+	{/if}
+{/each}
