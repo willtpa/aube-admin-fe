9d9a371bc62c2df173c7eef867a1fda1a62ed244
Alex <alexandre.greau@triple-a.io>
2024-05-27 14:21:41 +0800

    style: minimal homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h1>Messages</h1>
+<main>
+	<h1 class="mt-8">Welcome to Triple-A Backoffice</h1>
+	<ul>
+		<li class="card card-compact w-96 bg-base-100 shadow-xl">
+			<div class="card-body">
+				<h2 class="card-title">Currency Rates</h2>
+				<p>Keep an eye on LIVE currency rates</p>
+				<div class="card-actions justify-end">
+					<a href="/rates" class="btn btn-primary">See Rates</a>
+				</div>
+			</div>
+		</li>
+	</ul>
+</main>
+
+<style lang="scss">
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: flex-start;
+		padding-top: 72px;
+		height: 100vh;
+	}
+</style>
