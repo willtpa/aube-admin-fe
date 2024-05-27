661bc42f3635f209349aef4198b438c6880c1e3a
Mark Joseph Siaga <mark.siaga@triple-a.io>
2024-05-27 09:10:11 +0800

    feat: create customer UI

---
+++ src/routes/compliance/customer/+page.svelte
@@ -0,0 +1,50 @@
+<div class="mx-auto max-w-4xl">
+	<a href="customer/create" class="btn btn-primary mt-10">Create New</a>
+	<table class="table table-zebra mt-5">
+		<!-- head -->
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Type</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			<!-- row 1 -->
+			<tr>
+				<td>Acme 1</td>
+				<td>Corporate</td>
+				<td>
+					<button class="btn btn-xs">View</button>
+					<button class="btn btn-xs">Edit</button>
+				</td>
+			</tr>
+			<!-- row 2 -->
+			<tr>
+				<td>John Doe</td>
+				<td>Individual</td>
+				<td>
+					<button class="btn btn-xs">View</button>
+					<button class="btn btn-xs">Edit</button>
+				</td>
+			</tr>
+			<tr>
+				<td>Acme 2</td>
+				<td>Corporate</td>
+				<td>
+					<button class="btn btn-xs">View</button>
+					<button class="btn btn-xs">Edit</button>
+				</td>
+			</tr>
+			<!-- row 3 -->
+			<tr>
+				<td>Acme 3</td>
+				<td>Corporate</td>
+				<td>
+					<button class="btn btn-xs">View</button>
+					<button class="btn btn-xs">Edit</button>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
