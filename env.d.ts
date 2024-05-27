df85a0210350aadc911e5fde76692d29cab9f57d
Vincent Serpoul <vincent.serpoul@triple-a.io>
2024-05-27 13:39:36 +0800

    feat: declare env vars

---
+++ env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="@sveltejs/kit" />
+
+declare module '$env/static/public' {
+	export const PUBLIC_ADMINAPI_HOST: string;
+	// Add more environment variables as needed
+}
+
+declare module '$env/static/private' {
+	export const CF_ACCESS_CLIENT_SECRET: string;
+	export const CF_ACCESS_CLIENT_ID: string;
+	// Add more environment variables as needed
+}
