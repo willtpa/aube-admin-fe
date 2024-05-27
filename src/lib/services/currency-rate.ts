dfd11f40a0130f811dcc5b7ef59eee1c10d92135
Alex <alexandre.greau@triple-a.io>
2024-05-27 11:21:08 +0800

    chore: updated README + solved imports

--- src/lib/services/currency-rate.ts
+++ src/lib/services/currency-rate.ts
@@ -1,15 +1,13 @@
-import { PUBLIC_ADMINAPI_HOST } from '$env/static/public';
-import { CF_ACCESS_CLIENT_ID, CF_ACCESS_CLIENT_SECRET } from '$env/static/private';
 import { Requestor } from '$utils/http';
 import { type CurrencyCodeToMedianFxRateV1Map } from '$lib/services/currency-rate.d';
 
-const requestor = new Requestor(PUBLIC_ADMINAPI_HOST);
+const requestor = new Requestor(process.env.PUBLIC_ADMINAPI_HOST || '');
 
 export async function getRates(): Promise<CurrencyCodeToMedianFxRateV1Map> {
 	// set headers for cloudflare access
 	const headers = {
-		'CF-Access-Client-Id': CF_ACCESS_CLIENT_ID,
-		'CF-Access-Client-Secret': CF_ACCESS_CLIENT_SECRET,
+		'CF-Access-Client-Id': process.env.CF_ACCESS_CLIENT_ID || '',
+		'CF-Access-Client-Secret': process.env.CF_ACCESS_CLIENT_SECRET || '',
 	};
 
 	return requestor.get<CurrencyCodeToMedianFxRateV1Map>('/v1/admin/api/rates', { headers });
