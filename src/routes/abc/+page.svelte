3fc1796a6b77aa4ea6166c6097b281b4736ec64a
willtpa <willys.tjiawi@triple-a.io>
2024-06-22 17:50:14 +0800

    chore: test abc

--- src/routes/abc/+page.svelte
+++ src/routes/abc/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
     import { onMount } from 'svelte';
-    let mockData: { name: string; data: string }[];
+    let mockData: { name: string; data: string }[] = [{ name: 'sample', data: 'sample data' }];
     onMount(() => {
-        fetch('abc/')
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data);
-                mockData = [...data];
-            });
+        // fetch('abc/')
+        //     .then((res) => res.json())
+        //     .then((data) => {
+        //         console.log(data);
+        //         mockData = [...data];
+        //     });
         // .then(console.log);
         console.log('Page');
     });
