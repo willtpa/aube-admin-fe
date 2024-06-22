e2be0bd6c765b8fb5e7585698069f3c7dee05f24
willtpa <willys.tjiawi@triple-a.io>
2024-06-22 18:29:35 +0800

    chore: test abc

--- src/routes/abc/+server.ts
+++ src/routes/abc/+server.ts
@@ -1,28 +1,33 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async () => {
-    // const sseCurrencyRatesURL = `https://63744728716c2e191659ae70.mockapi.io/v1/willtest/test1`;
+    console.log('GET: RequestHandler is called........ ');
+    const sseCurrencyRatesURL = `https://63744728716c2e191659ae70.mockapi.io/v1/willtest/test1`;
 
-    // const result = await fetch(sseCurrencyRatesURL);
+    console.log('GET: RequestHandler is called........ sseCurrencyRatesURL: ', sseCurrencyRatesURL);
 
-    // if (!result.ok) {
-    //     return new Response(JSON.stringify({ error: 'Failed to fetch SSE' }), {
-    //         status: result.status,
-    //     });
-    // }
+    const result = await fetch(sseCurrencyRatesURL);
 
-    // return new Response(result.body, {
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //     },
-    // });
+    console.log('GET: RequestHandler is called........ result: ', JSON.stringify(result));
 
-    return new Response(
-        JSON.stringify([{ name: 'Helloworld', data: `sampledata Hello - ${new Date()}` }]),
-        {
-            headers: {
-                'Content-Type': 'application/json',
-            },
+    if (!result.ok) {
+        return new Response(JSON.stringify({ error: 'Failed to fetch SSE' }), {
+            status: result.status,
+        });
+    }
+
+    return new Response(result.body, {
+        headers: {
+            'Content-Type': 'application/json',
         },
-    );
+    });
+
+    // return new Response(
+    //     JSON.stringify([{ name: 'Helloworld', data: `sampledata Hello - ${new Date()}` }]),
+    //     {
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //     },
+    // );
 };
