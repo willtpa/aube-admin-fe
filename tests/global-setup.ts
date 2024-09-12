058b70bb62a74b6da0043acf820d43f75805ceea
willtpa <willys.tjiawi@triple-a.io>
2024-09-12 18:52:52 +0800

    chore: add mock server

--- tests/global-setup.ts
+++ tests/global-setup.ts
@@ -4,7 +4,7 @@ import { config as getEnv } from 'dotenv';
 async function globalSetup(_: FullConfig): Promise<void> {
     getEnv();
     process.env['PUBLIC_ADMINAPI_HOST'] = process.env['PUBLIC_ADMINAPI_HOST'] ?? '';
-    process.env['PUBLIC_ADMINAPI_PREFIX'] = 'v1/admin/api';
+    process.env['PUBLIC_ADMINAPI_PREFIX'] = 'admin/api/v1';
     process.env['PUBLIC_RATES_SSE_PREFIX'] = 'rates/sse';
     process.env['CF_ACCESS_CLIENT_ID'] = process.env['CF_ACCESS_CLIENT_ID'] ?? 'test-client-id';
     process.env['CF_ACCESS_CLIENT_SECRET'] =
